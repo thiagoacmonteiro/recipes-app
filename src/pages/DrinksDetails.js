db314f8a51321448301c568717073cdd1d9c2820
Diego Brito <diegorbrito9@gmail.com>
2021-12-13 19:27:51 -0300

    [GROUP 9] req 33 parcialmente concluido

--- src/pages/DrinksDetails.js
+++ src/pages/DrinksDetails.js
@@ -1,9 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { fetchById } from '../services/fetchApi';
 
 export default function DrinksDetails() {
+  const [drinks, setDrinks] = useState();
+  const {
+    location: { pathname },
+  } = useHistory();
+
+  const id = pathname.split('/')[2];
+
+  useEffect(() => {
+    fetchById('cocktail', id).then((response) => setDrinks(response.drinks[0]));
+  }, [id]);
+
+  const ingredients = drinks && Object.entries(drinks).reduce((acc, value) => {
+    if (value[0].includes('strIngredient') && value[1] !== null) {
+      acc.push(value[1]);
+    }
+    return acc;
+  }, []);
+
   return (
     <div>
-      <p> Drinks Details </p>
+      {drinks && (
+        <>
+          <img src={ drinks.strDrinkThumb } alt="" data-testid="recipe-photo" />
+          <p data-testid="recipe-title">{drinks.strDrink}</p>
+          <p data-testid="recipe-category">{drinks.strCategory}</p>
+
+          <ul>
+            {
+              ingredients.map((ingredient, index) => (
+                <li
+                  key={ ingredient }
+                  data-testid={ `${index}-ingredient-name-and-measure` }
+                >
+                  {ingredient}
+                </li>
+              ))
+            }
+          </ul>
+          <p data-testid="instructions">{drinks.strInstructions}</p>
+
+          <button type="button" data-testid="share-btn">
+            Compartilhar
+          </button>
+
+          <button type="button" data-testid="favorite-btn">
+            Favoritar
+          </button>
+
+          <button type="button" data-testid="start-recipe-btn">
+            Iniciar Receita
+          </button>
+        </>
+      )}
     </div>
   );
 }
--- src/pages/MealsDetails.js
+++ src/pages/MealsDetails.js
@@ -1,9 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { fetchById } from '../services/fetchApi';
 
 export default function MealsDetails() {
+  const [recipe, setRecipe] = useState();
+  const {
+    location: { pathname },
+  } = useHistory();
+
+  const id = pathname.split('/')[2];
+
+  useEffect(() => {
+    fetchById('meal', id).then((response) => setRecipe(response.meals[0]));
+  }, [id]);
+
+  const ingredients = recipe && Object.entries(recipe).reduce((acc, value) => {
+    if (value[0].includes('strIngredient') && value[1] !== '') {
+      acc.push(value[1]);
+    }
+    return acc;
+  }, []);
+
   return (
     <div>
-      <p> Meals Details </p>
+      {recipe && (
+        <>
+          <img src={ recipe.strMealThumb } alt="" data-testid="recipe-photo" />
+          <p data-testid="recipe-title">{recipe.strMeal}</p>
+          <p data-testid="recipe-category">{recipe.strCategory}</p>
+
+          <ul>
+            {
+              ingredients.map((ingredient, index) => (
+                <li
+                  key={ ingredient }
+                  data-testid={ `${index}-ingredient-name-and-measure` }
+                >
+                  {ingredient}
+                </li>
+              ))
+            }
+          </ul>
+          <p data-testid="instructions">{recipe.strInstructions}</p>
+          <a href={ recipe.strYoutube } data-testid="video">{recipe.strYoutube}</a>
+
+          <button type="button" data-testid="share-btn">
+            Compartilhar
+          </button>
+
+          <button type="button" data-testid="favorite-btn">
+            Favoritar
+          </button>
+
+          <button type="button" data-testid="start-recipe-btn">
+            Iniciar Receita
+          </button>
+        </>
+      )}
     </div>
   );
 }
