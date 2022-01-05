7bfe97983afe14092b3c2f1d5a63909fb25ea2f3
Arthur Simões <arthursimoes155@gmail.com>
2022-01-05 17:57:44 -0300

    [GROUP 9] req-53

--- src/components/FinishBtn.jsx
+++ src/components/FinishBtn.jsx
@@ -1,8 +1,14 @@
 import PropTypes from 'prop-types';
 import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 function FinishBtn({ ingredients, localIngredients }) {
   const [disabled, setDisabled] = useState(true);
+  const history = useHistory();
+
+  function handleClick() {
+    history.push('/receitas-feitas');
+  }
 
   useEffect(() => {
     if (ingredients.length !== 0 && localIngredients.length === ingredients.length) {
@@ -16,6 +22,7 @@ function FinishBtn({ ingredients, localIngredients }) {
     <button
       type="button"
       data-testid="finish-recipe-btn"
+      onClick={ handleClick }
       disabled={ disabled }
     >
       Finish
